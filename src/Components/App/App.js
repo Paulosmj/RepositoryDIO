3f5deb4c56cf6b613a3ee342c5c220b18bbb9674
Paulosmj <psmeneguesso@gmail.com>
2022-04-19 18:17:59 -0300

    useState e useEffect

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import IFoodCounter from '../iFoodCounter/iFoodCounter';
+
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <IFoodCounter />
+    </>
+  );
+}
+
+export default App;
